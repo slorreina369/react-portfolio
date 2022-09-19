73504c724561412afdc07cba00202f44bfbfacdf
lorreina <slorreina369@gmail.com>
2022-09-19 10:05:58 -0700

    created and styled and spaced footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,9 @@ function Footer(){
         <footer>
             <div>
                 <h2>Reach out and learn more</h2>
-                <a href="https://github.com/slorreina369"><img src={LogoGit} alt="Github logo" /></a>
+                <a className="footerLogo" href="https://github.com/slorreina369"><img src={LogoGit} alt="Github logo" /></a>
 
-                <a href="https://www.linkedin.com/in/lorreina-guyett-261babb2/"><img src={LogoLink} alt="LinkedIn logo" /></a>
+                <a className="footerLogo" href="https://www.linkedin.com/in/lorreina-guyett-261babb2/"><img src={LogoLink} alt="LinkedIn logo" /></a>
             </div>
         </footer>
     )
