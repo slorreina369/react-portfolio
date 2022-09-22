ffb0b38ad6077c549f025c3c90ac90116f55a146
lorreina <slorreina369@gmail.com>
2022-09-21 23:05:22 -0700

    resume page updated

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -11,6 +11,23 @@ function Resume(){
                 slorrein369@gmail.com</p>
             </div>
             Download my <a href={ResumeFile}>Resume</a>
+            <ul>
+                <p>Front-End</p>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JQuery</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+                <li>React</li>
+            </ul>
+            <ul>
+                <p>Back-end</p>
+                <li>APIs</li>
+                <li>Express</li>
+                <li>Node</li>
+                <li>MySQL, Sequelize</li>
+                <li>GraphQL</li>
+            </ul>
         </section>
     )
 };
